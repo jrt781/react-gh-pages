c0de65322b9a8d4062ed19b043db3dbc62a80f70
jaketyler <jrtyler@byu.edu>
2019-01-25 14:57:49 -0700

    Changed AboutPage to open GitHub repo in new tab

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -9,7 +9,7 @@ class AboutPage extends React.Component {
 			<div style={{padding: '2rem 5rem'}}>
 				<h1>About</h1>
 				<p>This application uses React, React Router, and a variety of other helpful libraries.</p>
-				<p>Check out the <a href={'https://github.com/jrt781/react-gh-pages'}>GitHub repository</a> to look at
+				<p>Check out the <a href={'https://github.com/jrt781/react-gh-pages'} target="_blank" >GitHub repository</a> to look at
 					the code and see some helpful instructions</p>
 			</div>
 		);
