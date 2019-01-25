630dd9390f8c72f431013dcb840b44cd7affbed6
jaketyler <jrtyler@byu.edu>
2019-01-25 09:56:00 -0700

    Updates

---
+++ precache-manifest.677092152ed43c5eba8d460a2039fd9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bb352a07584cf794f82",
+    "url": "/react-gh-pages/static/css/main.cf36e73d.chunk.css"
+  },
+  {
+    "revision": "8bb352a07584cf794f82",
+    "url": "/react-gh-pages/static/js/main.8bb352a0.chunk.js"
+  },
+  {
+    "revision": "c3c1078cdcac66d80673",
+    "url": "/react-gh-pages/static/css/1.caafb79c.chunk.css"
+  },
+  {
+    "revision": "c3c1078cdcac66d80673",
+    "url": "/react-gh-pages/static/js/1.c3c1078c.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "dcc2c62c8f2781d84c7e2a03a9d22822",
+    "url": "/react-gh-pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.5835fed6368cd8e5f40e68e5b66bfd54.js"
+  "/react-gh-pages/precache-manifest.677092152ed43c5eba8d460a2039fd9f.js"
 );
 
 workbox.clientsClaim();
